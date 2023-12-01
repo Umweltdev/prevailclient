050ad38404e79692e6d88127c92096eedc237797
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-12-01 20:59:40 +0100

    portfolio text change

--- src/components/Portfolio/SectionOne.jsx
+++ src/components/Portfolio/SectionOne.jsx
@@ -11,15 +11,25 @@ const SectionOne = () => {
         <div className="sOnePortSection">
           <img
             className="sOnePortImage"
-            src="https://images.unsplash.com/photo-1527192491265-7e15c55b1ed2?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src="https://plus.unsplash.com/premium_photo-1681488262364-8aeb1b6aac56?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
           />
 
           <div className="sOnePortText">
-            <h1>Customer-Centric Approach</h1>
-            <h1>Innovation and Creativity</h1>
-            <h1>Building Brand Authority</h1>
-            <h1>Social Impact through Digital Outreach</h1>
+            <h1>
+              <span style={{ color: "white" }}>Cu</span>stomer-Centric Approach
+            </h1>
+            <h1>
+              <span style={{ color: "white" }}>Inn</span>ovation and Creativity
+            </h1>
+            <h1>
+              <span style={{ color: "white" }}>Bu</span>ilding Brand Authority
+            </h1>
+            <h1>
+              {" "}
+              <span style={{ color: "white" }}>So</span>cial Impact through
+              Digital <span style={{ color: "white" }}>Ou</span>treach
+            </h1>
           </div>
         </div>
       </div>
