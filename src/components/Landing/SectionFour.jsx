1bd5d622a06b4d00424ba1bee76c4fed42a52af0
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-01-19 15:12:08 +0100

    contact

--- src/components/Landing/SectionFour.jsx
+++ src/components/Landing/SectionFour.jsx
@@ -7,7 +7,7 @@ const SectionFour = () => {
       <ComponentContact
         img="https://images.unsplash.com/photo-1520333789090-1afc82db536a?q=80&w=1471&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         header="Is Your Mind Open For Change?"
-        text=" Lorem ipsum dolor sit amet consectetur adipisicing elit"
+        text="Prevail’s voice is professional yet approachable, combining expertise with a friendly demeanour. We aim to communicate with clarity, simplicity, and authenticity, delivering our messages in a way that resonates with our clients and builds trust. Because truly understanding their goals, challenges, and aspirations would allow us to develop customisable strategies that yield tangible results. As a result, we will constantly grow, develop, and learn with our clients because we are empowered by knowledge and being the better version of ourselves for our clients."
       />
     </div>
   );
