dd955e849caf79396d6e13aca0d757f609391030
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-01-30 16:32:40 +0100

    our packages section

--- src/components/Services/SectionFour.jsx
+++ src/components/Services/SectionFour.jsx
@@ -1,6 +1,41 @@
 import "./assets/services.css";
+import React, { useState } from "react";
+import { EstateSilver, cusExecItem, customItems, shopifyGold, shopifySilver } from "../Portfolio/assets/PortfolioData";
+import { ArrowDownward } from "@mui/icons-material";
 
 const SectionFour = () => {
+  const [shopifySilverItems, setShopifySilverItems] = useState(5);
+  const [shopifyGoldItems, setShopifyGoldItems] = useState(5);
+  const [shopifyCustomItems, setShopifyCustomItems] = useState(5);
+  const [shopifyExecItems, setShopifyExecItems] = useState(5);
+  const [shopifyExecCusItems, setShopifyExecCusItems] = useState(5);
+
+  const handleLoadSilver = () => {
+    // Increase the number of items to display
+    setShopifySilverItems(shopifySilverItems + 5);
+  };
+
+  const handleLoadGold = () => {
+    // Increase the number of items to display
+    setShopifyGoldItems(shopifyGoldItems + 5);
+  };
+
+  const handleLoadCustom = () => {
+    // Increase the number of items to display
+    setShopifyCustomItems(shopifyCustomItems + 5);
+  };
+
+  const handleLoadExec = () => {
+    // Increase the number of items to display
+    setShopifyExecItems(shopifyExecItems + 5);
+  };
+
+   const handleLoadExecCus = () => {
+     // Increase the number of items to display
+     setShopifyExecCusItems(shopifyExecCusItems + 5);
+   };
+
+
   return (
     <div className="sFourServiceContainer">
       <h1 className="sFourServiceHeader">Our Packages</h1>
@@ -10,89 +45,41 @@ const SectionFour = () => {
           <div className="sFourServiceCardTwo">
             <h1 className="sFourServiceCardTwo1">Shopify Silver Package</h1>
             <p className="sFourServiceCardSpanText">Original €2,000 once-off</p>
-            {/* <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1> */}
             <ul className="sFourServiceCardList">
-              <li>SSL certificate</li>
-              <li>Domain set-up & hosting</li>
-              <li>Admin login</li>
-              <li>Basic website customisation</li>
-              <li>Website transfer</li>
-              <li>404 pages fix/redirect</li>
-              <li>W3C certified HTML 5,CCS 3 & JavaScript UI</li>
-              <li>5 main pages & sub-pages</li>
-              <li>Search bar</li>
-              <li>Shopping cart</li>
-              <li>Wishlist/save</li>
-              <li>Navbar</li>
-              <li>SideBar</li>
-              <li>review & rating</li>
-              <li>Social media plugin integration</li>
-              <li>Blog page layout</li>
-              <li>3rd party API integrations (additional payment)</li>
-              <li>Responsive design (any device)</li>
-              <li>Admin login (edit /review allowance)</li>
-              <li>Technical support</li>
-              <li>Brainstorm session</li>
-              <li>1 revision</li>
+              {shopifySilver.slice(0, shopifySilverItems).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
+            {shopifySilverItems < 30 && ( // Assuming you have a maximum of 30 items
+              <button
+                className="sFourServiceLoadMore"
+                onClick={handleLoadSilver}
+              >
+                Load More{" "}
+                <ArrowDownward sx={{ color: "#0076ff", fontSize: "1vw" }} />
+              </button>
+            )}
             <p className="sFourServiceCardBottom">Original €2,000 once-off</p>
             <button className="sFourServiceCardBtn">Get A Qoute</button>
           </div>
         </div>
-
-        {/* MAIN */}
-        {/* <div className="sFourServiceCard">
-          <div className="sFourServiceCardOne">
-            <p>WE RECOMMEND</p>
-          </div>
-          <div className="sFourServiceCardTwo">
-            <h1 className="sFourServiceCardTwo1"> SEO Package 2</h1>
-            <p className="sFourServiceCardSpanText">
-              min 6-12 months recommended
-            </p>
-            <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1>
-            <ul className="sFourServiceCardList">
-              <li>Initial Review, Audit & Analysis</li>
-              <li>Competitor Analysis</li>
-              <li>On-Page SEO based on reports </li>
-              <li>Local Search Optimisation</li>
-              <li>GMB Optimisation</li>
-              <li>Google Analytics Tracking</li>
-              <li>Google 1st page (5 key phrases)</li>
-              <li>Backlinks Consultation/ Generation</li>
-              <li>New Blogpost optimised x 2</li>
-              <li>Weekly Reporting</li>
-              <li>Customer Support</li>
-              <p className="sFourServiceCardBottomMain">
-                Starting at €795 per month
-              </p>
-              <button className="sFourServiceCardBtn">Get A Qoute</button>
-            </ul>
-          </div>
-        </div> */}
 
         {/* TWO */}
         <div className="sFourServiceCardSmall">
           <div className="sFourServiceCardTwo">
             <h1 className="sFourServiceCardTwo1">Shopify Gold Package</h1>
             <p className="sFourServiceCardSpanText">Original €3,000 once-off</p>
-            {/* <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1> */}
             <ul className="sFourServiceCardList">
-              <li>Everything in the “Shopify Standard Package”</li>
-              <li>Advanced website customisation</li>
-              <li>Advanced UI</li>
-              <li>FAQ section</li>
-              <li>Up to 10 pages</li>
-              <li>Website development presentation</li>
-              <li>Email set-up;</li>
-              <li>Website content review</li>
+              {shopifyGold.slice(0, shopifyGoldItems).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
+            {shopifyGoldItems < shopifyGold.length && ( // Assuming you have a maximum of 30 items
+              <button className="sFourServiceLoadMore" onClick={handleLoadGold}>
+                Load More{" "}
+                <ArrowDownward sx={{ color: "#0076ff", fontSize: "1vw" }} />
+              </button>
+            )}
             <p className="sFourServiceCardBottom">Original €3,000 once-off</p>
             <button className="sFourServiceCardBtn">Get A Qoute</button>
           </div>
@@ -105,40 +92,20 @@ const SectionFour = () => {
               Custom Website Complete Package{" "}
             </h1>
             <p className="sFourServiceCardSpanText">Consultation Meeting</p>
-            {/* <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1> */}
             <ul className="sFourServiceCardList">
-              <li>SSL certificate</li>
-              <li>Domain set-up & hosting</li>
-              <li>Admin login</li>
-              <li>Advanced website customisation</li>
-              <li>Meta & title</li>
-              <li>Website transfer</li>
-              <li>404 pages fix/redirect</li>
-              <li>W3C certified HTML 5,CSS3 & JAVSCRIPT</li>
-              <li>advanced UI</li>
-              <li>Up to 10 pages</li>
-              <li>Banner/slideshow</li>
-              <li>Search bar</li>
-              <li>Shopping cart</li>
-              <li>Wishlist/save</li>
-              <li>navbar</li>
-              <li>FAQ section</li>
-              <li>review & rating</li>
-              <li>Sidebar</li>
-              <li>email set-up</li>
-              <li>blog page layout</li>
-              <li>Social media plugin integration</li>
-              <li>3rd party API integrations (additional payment)</li>
-              <li>Responsive design (any device)</li>
-              <li>Technical support</li>
-              <li>Brainstorm session</li>
-              <li>Buy now function</li>
-              <li>website content review</li>
-              <li>Website development presentation</li>
-              <li>1 revision</li>
+              {customItems.slice(0, shopifyCustomItems).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
+            {shopifyCustomItems < customItems.length && ( // Assuming you have a maximum of 30 items
+              <button
+                className="sFourServiceLoadMore"
+                onClick={handleLoadCustom}
+              >
+                Load More{" "}
+                <ArrowDownward sx={{ color: "#0076ff", fontSize: "1vw" }} />
+              </button>
+            )}
             <p className="sFourServiceCardBottom">
               Custom Website Complete Package
             </p>
@@ -146,53 +113,50 @@ const SectionFour = () => {
           </div>
         </div>
 
+        {/* FOUR */}
         <div className="sFourServiceCardSmall">
           <div className="sFourServiceCardTwo">
             <h1 className="sFourServiceCardTwo1">
               Custom Website Executive Package{" "}
             </h1>
             <p className="sFourServiceCardSpanText">Consultation</p>
-            {/* <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1> */}
             <ul className="sFourServiceCardList">
-              <li>Everything in the “Custom Website Complete package”</li>
-              <li>tag description optimisations</li>
-              <li>dynamic UI & React</li>
-              <li>Up to 20 pages</li>
-              <li>Product comparison</li>
-              <li>Website transfer</li>
-              <li>Privacy policy & terms and conditions page</li>
-              <li>Order invoice</li>
-              <li>Online booking system</li>
-              <li>Calendar booking system</li>
-              <li>newsletter sign-up</li>
-              <li>2 revisions</li>
+              {cusExecItem.slice(0, shopifyExecItems).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
+            {shopifyExecItems < cusExecItem.length && ( // Assuming you have a maximum of 30 items
+              <button className="sFourServiceLoadMore" onClick={handleLoadExec}>
+                Load More{" "}
+                <ArrowDownward sx={{ color: "#0076ff", fontSize: "1vw" }} />
+              </button>
+            )}
             <p className="sFourServiceCardBottom">Consultancy</p>
             <button className="sFourServiceCardBtn">Get A Qoute</button>
           </div>
         </div>
 
+        {/* FIVE */}
         <div className="sFourServiceCardSmall exec">
           <div className="sFourServiceCardTwo">
             <h1 className="sFourServiceCardTwo1">
               Custom Website Executive Package{" "}
             </h1>
             <p className="sFourServiceCardSpanText">Consultation</p>
-            {/* <h1 className="sFourServiceCardTwo2">
-              For Middle-Sized Companies & E-Commerce
-            </h1> */}
             <ul className="sFourServiceCardList">
-              <li>Everything in the “Estate Agency Complete package”</li>,
-              <li>tag description optimisations</li>,<li>dynamic UI & React</li>
-              ,<li> property comparison</li>,<li>up to 20 pages</li>,
-              <li>website development presentation</li>,
-              <li>privacy policy & terms and conditions page</li>,
-              <li>order invoice</li>,<li>online booking system</li>,
-              <li>calendar booking system</li>,<li>newsletter sign-up</li>,
-              <li>2 revisions</li>,
+              {EstateSilver.slice(0, shopifyExecCusItems).map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
             </ul>
+            {shopifyExecCusItems < EstateSilver.length && (
+              <button
+                className="sFourServiceLoadMore"
+                onClick={handleLoadExecCus}
+              >
+                Load More{" "}
+                <ArrowDownward sx={{ color: "#0076ff", fontSize: "1vw" }} />
+              </button>
+            )}
             <p className="sFourServiceCardBottom">Consultancy</p>
             <button className="sFourServiceCardBtn">Get A Qoute</button>
           </div>
