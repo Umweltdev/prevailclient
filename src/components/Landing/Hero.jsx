d196916e994971b328bc3f55defaad68f646f301
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-02-28 21:55:25 +0100

    landing | landing waterflow fixed

--- src/components/Landing/Hero.jsx
+++ src/components/Landing/Hero.jsx
@@ -84,7 +84,7 @@ const Hero = () => {
     <div className="hero1">
       <spline-viewer
         className="spline-Layer"
-        url="https://prod.spline.design/Nb2I5a6X-Kr8fdit/scene.splinecode"
+        url="#"
       ></spline-viewer>
       <motion.div
         initial={{ opacity: 0, height: 0 }}
