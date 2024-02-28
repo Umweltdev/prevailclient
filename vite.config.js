14d4fcb4141a63972ddf6215d921129618711db8
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-02-28 12:57:11 +0100

    landing | button fixed

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,12 +8,14 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
+          if (id.indexOf("node_modules") !== -1) {
+            const basic = id.toString().split("node_modules/")[1];
+            const sub1 = basic.split("/")[0];
+            if (sub1 !== ".pnpm") {
+              return sub1.toString();
+            }
+            const name2 = basic.split("/")[1];
+            return name2.split("@")[name2[0] === "@" ? 1 : 0].toString();
           }
         },
       },
