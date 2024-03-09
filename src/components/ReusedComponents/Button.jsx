12750ffc09b083fa96b49b9823f75d383e31de3f
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-03-09 10:47:39 +0100

    mobile flex across all pages

--- src/components/ReusedComponents/Button.jsx
+++ src/components/ReusedComponents/Button.jsx
@@ -18,11 +18,13 @@ function BasicButtons({ text, gradient, width }) {
             "0px 3px 5px -1px rgba(255,105,135,0.5), 0px 6px 10px 0px rgba(255,105,135,0.3), 0px 1px 18px 0px rgba(255,105,135,0.2)",
 
           "@media (max-width: 600px)": {
-            padding: "0.4vh 2vw",
+            padding: "0.5vh 6vw",
             borderRadius: "5vw",
             color: "white",
             fontWeight: "unset",
-            fontSize: "3vw"
+            fontSize: "3vw",
+            boxShadow: "unset",
+            textAlign: "center"
           },
         }}
       >
