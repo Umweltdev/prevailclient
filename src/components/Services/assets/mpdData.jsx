b62685ab4bc983b4f0982f37b4c8ccf5a08a4b05
sirlahm <riliwanademola@yahoo.com>
2024-06-26 07:38:16 +0100

    update

--- src/components/Services/assets/mpdData.jsx
+++ src/components/Services/assets/mpdData.jsx
@@ -5,13 +5,16 @@ export const slides = [
       "Bringing the magic of yield management and automated price adjustments based on models of elasticity.",
     image:
       "https://img.freepik.com/premium-photo/stack-coins-against-black-background_1048944-10931623.jpg?w=740",
+    data: ["Web Design", "Web Development"],
   },
+
   {
     topic: "Labour Effect",
     description:
       "Ability to increase efficiency, reduced cost, and enhance competitiveness.",
     image:
       "https://img.freepik.com/free-photo/medium-shot-woman-working-as-lawyer_23-2151152387.jpg?w=740",
+    data: ["Branding", "Web Development"],
   },
   {
     topic: "Tailored Redistribution Frequency",
@@ -19,6 +22,7 @@ export const slides = [
       "Choose how often to adjust your marketing spend (daily, weekly, monthly, quarterly) for strategic flexibility.",
     image:
       "https://img.freepik.com/premium-photo/overhead-view-young-woman-holding-pen-writing-notebook_35674-14486.jpg?w=740",
+    data: ["Advertising", "Marketing"],
   },
   {
     topic: "Digital Connection",
@@ -26,6 +30,7 @@ export const slides = [
       "Ability to connect to a digital Ads platform and database through an API.",
     image:
       "https://img.freepik.com/premium-photo/group-people-talking-social-network-3d-render_1379-1783.jpg?w=900",
+    data: ["User Interface", "User Experience"],
   },
   {
     topic: "Holistic Expense Tracking",
@@ -33,6 +38,7 @@ export const slides = [
       "Gain a comprehensive understanding of all marketing expenses and their impact",
     image:
       "https://img.freepik.com/premium-photo/investor-is-looking-stock-charts-mobile-phones-tablets-he-is-stock-investor-he-trades-stocks-by-analyzing-charts-using-indicators-enter-trades-stock-investment-idea_528263-1849.jpg?w=740",
+    data: ["IT", "Networking"],
   },
   {
     topic: "Price Estimator",
@@ -40,6 +46,7 @@ export const slides = [
       "Gain insightful price estimator quotations on an admin frontend portal built by Prevail or sent as a spreadsheet pdf.",
     image:
       "https://img.freepik.com/premium-photo/male-businessman-works-with-business-reports-financial-charts-paperwork-late-night_361425-5147.jpg?w=740",
+    data: ["Branding", "Marketing"],
   },
   {
     topic: "Version Update",
@@ -47,5 +54,7 @@ export const slides = [
       "Customers who purchase version 1 will get first access to version 2",
     image:
       "https://img.freepik.com/free-photo/student-boy-using-laptop-learning-online-education_1150-16903.jpg?w=740",
+      data: ["Branding", "Marketing"],
+
   },
 ];
