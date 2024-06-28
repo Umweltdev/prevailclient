d3d7cc582689f78a0ae65a2a44a5fc375c5cc967
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-06-28 11:19:55 +0100

    code splitting

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,56 @@
-import Landing from "./pages/Landing";
-import About from "./pages/About";
+import React, { Suspense, useContext } from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
-import Portfolio from "./pages/Portfolio";
-import Blog from "./pages/Blog";
-import Services from "./pages/Services";
-import Contact from "./pages/Contact";
-import BlogDetails from "./components/Blog/BlogDetails";
-import BrandIdentity from "./components/Services/BrandIdentity/BrandIdentity";
-import WebsiteDevelopment from "./components/Services/WebsiteDevelopement/WebsiteDevelopment";
-import WebManagement from "./components/Services/WebManagement/WebManagement";
-import Sem from "./components/Services/Sem/Sem";
-import Mpd from "./components/Services/Mpd/Mpd";
-import DigitalAccelerator from "./components/Services/DigitalAccelerator/DigitalAccelerator";
-import DigitalEcosystem from "./components/Explore/DigitalEcosystem/DigitalEcosystem";
-import CustomerJourney from "./components/Explore/CustomerJourney/CustomerJourney";
-import AboutOurWhy from "./components/About/AboutOurWhy";
-import AboutOurSolution from "./components/About/AboutOurSolution";
-import CaseDetails from "./components/Portfolio/CaseDetails";
-import SignUpForm from "./pages/SignUp";
-import LoginForm from "./pages/Login";
-
-import Stepper from "./pages/Stepper";
-import MobStepper from "./pages/MobStepper";
-import UserDashBoard from "./pages/UserDashboard";
-import Profile from "./components/user-dashboard/Profile";
-
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
-import PrivacyPolicy from "./pages/PrivacyPolicy";
 import ScrollToTop from "./ScrollToTop";
 import { AuthContext } from "./context/AuthContext";
-import { useContext } from "react";
-import ResetPasswordForm from "./components/Form/ForgotPassword/ResetPasswordForm";
+import Loading from "./components/utils/Loading";
+
+// Lazy load pages and components
+const Landing = React.lazy(() => import("./pages/Landing"));
+const About = React.lazy(() => import("./pages/About"));
+const Portfolio = React.lazy(() => import("./pages/Portfolio"));
+const Blog = React.lazy(() => import("./pages/Blog"));
+const Services = React.lazy(() => import("./pages/Services"));
+const Contact = React.lazy(() => import("./pages/Contact"));
+const BlogDetails = React.lazy(() => import("./components/Blog/BlogDetails"));
+const BrandIdentity = React.lazy(() =>
+  import("./components/Services/BrandIdentity/BrandIdentity")
+);
+const WebsiteDevelopment = React.lazy(() =>
+  import("./components/Services/WebsiteDevelopement/WebsiteDevelopment")
+);
+const WebManagement = React.lazy(() =>
+  import("./components/Services/WebManagement/WebManagement")
+);
+const Sem = React.lazy(() => import("./components/Services/Sem/Sem"));
+const Mpd = React.lazy(() => import("./components/Services/Mpd/Mpd"));
+const DigitalAccelerator = React.lazy(() =>
+  import("./components/Services/DigitalAccelerator/DigitalAccelerator")
+);
+const DigitalEcosystem = React.lazy(() =>
+  import("./components/Explore/DigitalEcosystem/DigitalEcosystem")
+);
+const CustomerJourney = React.lazy(() =>
+  import("./components/Explore/CustomerJourney/CustomerJourney")
+);
+const AboutOurWhy = React.lazy(() => import("./components/About/AboutOurWhy"));
+const AboutOurSolution = React.lazy(() =>
+  import("./components/About/AboutOurSolution")
+);
+const CaseDetails = React.lazy(() =>
+  import("./components/Portfolio/CaseDetails")
+);
+const SignUpForm = React.lazy(() => import("./pages/SignUp"));
+const LoginForm = React.lazy(() => import("./pages/Login"));
+const Stepper = React.lazy(() => import("./pages/Stepper"));
+const MobStepper = React.lazy(() => import("./pages/MobStepper"));
+const UserDashBoard = React.lazy(() => import("./pages/UserDashboard"));
+const Profile = React.lazy(() => import("./components/user-dashboard/Profile"));
+const PrivacyPolicy = React.lazy(() => import("./pages/PrivacyPolicy"));
+const ResetPasswordForm = React.lazy(() =>
+  import("./components/Form/ForgotPassword/ResetPasswordForm")
+);
 
 const stripePromise = loadStripe(
   "pk_test_51OsCJ5P1A39VkufThp1PVDexesvf2XAY8faTyK0uucC1qRl9NW9QkpBdwXQDyjCAjzL166zjMWNn5Zr25ZkaQJVi00vurq61mj"
@@ -43,6 +62,7 @@ function App() {
   return (
     <Elements stripe={stripePromise}>
       <ScrollToTop />
+<<<<<<< HEAD
       <Routes>
         <Route path="/" element={<Landing />} />
         <Route path="/about-us" element={<About />} />
@@ -83,6 +103,56 @@ function App() {
           element={user ? <UserDashBoard /> : <Navigate to="/" />}
         />
       </Routes>
+=======
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route
+            path="/reset_password/:id/:token"
+            element={<ResetPasswordForm />}
+          />
+          <Route path="/About" element={<About />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/Blog" element={<Blog />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/Blog/:index" element={<BlogDetails />} />
+          <Route path="/Services" element={<Services />} />
+          <Route path="/Services/brand" element={<BrandIdentity />} />
+          <Route path="/Services/website" element={<WebsiteDevelopment />} />
+          <Route
+            path="/explore/digitalecosystem"
+            element={<DigitalEcosystem />}
+          />
+          <Route
+            path="/explore/customerjourney"
+            element={<CustomerJourney />}
+          />
+          <Route path="/Services/webmanagement" element={<WebManagement />} />
+          <Route path="/Services/privacypolicy" element={<PrivacyPolicy />} />
+          <Route path="/Services/sem" element={<Sem />} />
+          <Route path="/Services/mpd" element={<Mpd />} />
+          <Route
+            path="/Services/digitalaccelerator"
+            element={<DigitalAccelerator />}
+          />
+          <Route path="/Portfolio/:index" element={<CaseDetails />} />
+          <Route path="/about/ourWhy" element={<AboutOurWhy />} />
+          <Route path="/about/ourSolution" element={<AboutOurSolution />} />
+          <Route path="/SignUp" element={<SignUpForm />} />
+          <Route path="/Login" element={<LoginForm />} />
+          <Route path="/Stepper" element={<Stepper />} />
+          <Route path="/MobStepper" element={<MobStepper />} />
+          <Route
+            path="/UserDashboard"
+            element={user ? <UserDashBoard /> : <Navigate to="/" />}
+          />
+          <Route
+            path="/user/*"
+            element={user ? <UserDashBoard /> : <Navigate to="/" />}
+          />
+        </Routes>
+      </Suspense>
+>>>>>>> 0b18381 (code splitting)
     </Elements>
   );
 }
