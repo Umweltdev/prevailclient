b8c3d2183f6c86fd59525bb52d909e80f469637e
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-02 23:42:14 +0100

    intersection api

--- src/components/Landing/SectionSeven.jsx
+++ src/components/Landing/SectionSeven.jsx
@@ -1,12 +1,29 @@
-import { Box, Button, CardMedia, Grid, Typography } from "@mui/material";
+import { Box, CardMedia, Grid, Typography } from "@mui/material";
 import React from "react";
-import imago from "./home/DAB-Rocket.webp"
+import imago from "./home/DAB-Rocket.webp";
 import CardNoBorder from "./assets/CardNoBorder";
 import { DigitalAcelerator } from "./assets/LandingData";
 import ReusedButton from "../ReusedComponents/ReusedButton";
 import { Link } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import styles from "./assets/animation.module.css"; // Import the animation CSS
 
 const FAQ = () => {
+  const { ref: titleRef, inView: titleInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const { ref: textRef, inView: textInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
+  const { ref: imageRef, inView: imageInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1,
+  });
+
   return (
     <Box
       sx={{
@@ -33,6 +50,8 @@ const FAQ = () => {
       >
         <Typography
           sx={{ color: "#6E3EF4", fontSize: "24px", fontWeight: "400" }}
+          ref={titleRef}
+          className={titleInView ? styles.fadeInUp : ""}
         >
           Product
         </Typography>
@@ -48,8 +67,10 @@ const FAQ = () => {
               fontSize: "32px",
             },
           }}
+          ref={textRef}
+          className={textInView ? styles.fadeInUp : ""}
         >
-          Digital Accelerator Bundle bring Agility to Your Enterprise
+          Digital Accelerator Bundle brings Agility to Your Enterprise
         </Typography>
         <Typography
           sx={{
@@ -63,13 +84,15 @@ const FAQ = () => {
               fontSize: "16px",
             },
           }}
+          ref={textRef}
+          className={textInView ? styles.fadeInUp : ""}
         >
-          Discover how our Digital Accelerator Bundle package serves as your strategic 
-          gateway to digital success. This exclusive, comprehensive solution is crafted 
-          to enhance your online visibility and impact through three essential services: 
-          Website Development, Brand Identity, and Search Engine Optimisation (SEO).
+          Discover how our Digital Accelerator Bundle package serves as your
+          strategic gateway to digital success. This exclusive, comprehensive
+          solution is crafted to enhance your online visibility and impact
+          through three essential services: Website Development, Brand Identity,
+          and Search Engine Optimisation (SEO).
         </Typography>
-
         <Link to={"/services/digitalaccelerator"}>
           <ReusedButton text={"Learn More"} />
         </Link>
@@ -79,7 +102,6 @@ const FAQ = () => {
           display: "flex",
           gap: "70px",
           background: "#F9FAFC",
-
           "@media (max-width: 600px)": {
             width: "90vw",
             margin: "auto",
@@ -113,6 +135,8 @@ const FAQ = () => {
                 fontSize: "24px",
               },
             }}
+            ref={textRef}
+            className={textInView ? styles.fadeInUp : ""}
           >
             Why Opt for Our Digital Accelerator Bundle Package?
           </Typography>
@@ -129,12 +153,14 @@ const FAQ = () => {
                 header={data.header}
                 text={data.text}
                 icon={data.icon}
+                key={i}
+                ref={textRef}
+                className={textInView ? styles.fadeInUp : ""}
               />
             ))}
           </Box>
-          {/* <AccordionUsage /> */}
         </Grid>
-        <Grid>
+        <Grid ref={imageRef} className={imageInView ? styles.fadeInDown : ""}>
           <CardMedia
             component={"img"}
             image={imago}
