853c79ba721979c648677a15b66c3176fedfc6d1
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-03 10:10:39 +0100

    image cutdown

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,21 +1,14 @@
 import React, { useContext } from "react";
 import { Helmet } from "react-helmet";
-import Footer from "../components/Footer/Footer";
 import FooterNew from "../components/Footer/FooterNew";
-import CarouselOne from "../components/Landing/CarouselOne";
 import Hero from "../components/Landing/Hero";
 import OurCore from "../components/Landing/OurCore";
-import SectionFive from "../components/Landing/SectionFive";
 import SectionFour from "../components/Landing/SectionFour";
 import SectionSeven from "../components/Landing/SectionSeven";
-import SectionThree from "../components/Landing/SectionThree";
 import SectionTwo from "../components/Landing/SectionTwo";
 import CoreValues from "../components/Landing/CoreValues";
-import DrawerAppBarWhite from "../components/Navbar/Appbar";
-import AppBarOne from "../components/Navbar/AppbarOne";
 import { AuthContext } from "../context/AuthContext";
 import AppBarNav from "../components/Navbar/Appbar";
-import Testimonials from "../components/Landing/Testimonials";
 import JoinCustomers from "../components/About/JoinOurCustomer";
 
 const Landing = (props) => {
@@ -41,7 +34,6 @@ const Landing = (props) => {
         <SectionFour />
         <br />
         <br />
-        {/* <Testimonials /> */}
         <JoinCustomers />
         <FooterNew />
       </div>
