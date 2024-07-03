611c3be2ad0c297681aa2ac85f84875b4d47f7ec
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-03 15:51:11 +0100

    digital acceleraor page optimized

--- src/components/Explore/CustomerJourney/CustomerJourney.jsx
+++ src/components/Explore/CustomerJourney/CustomerJourney.jsx
@@ -1,35 +1,35 @@
-import React from 'react'
+import React from "react";
 import { Helmet } from "react-helmet";
 import DrawerAppBarWhite from "../../Navbar/Appbar";
-import FooterNew from '../../Footer/FooterNew';
-import Hero from './Hero';
-import Trigger from './Trigger';
+import FooterNew from "../../Footer/FooterNew";
+import Hero from "./Hero";
+import Trigger from "./Trigger";
 //import Presence from './Presence';
 //import Evaluation from './Evaluation';
 //import Purchase from './Purchase';
 //import SpecialOffer from './SpecialOffer';
-import Last from './Last';
+import Last from "./Last";
 
 const CustomerJourney = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Transform Your Customer Journey with Prevail</title>
         <meta
           name="description"
           content="Map and Optimise Every Stage of Your Customer Journey for Growth"
         />
       </Helmet>
-    <div style={{ width: "100%", overflow: "hidden" }}>
+      <div style={{ width: "100%", overflow: "hidden" }}>
         <DrawerAppBarWhite />
-        
+
         <Hero />
         <Trigger />
         <Last />
         <FooterNew />
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default CustomerJourney
+export default CustomerJourney;
