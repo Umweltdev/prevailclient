bdabb3923cb824b6ccbf181c7e385db6ed77fc88
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-03 11:21:50 +0100

    contact detsructure

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,39 +1,23 @@
 import { Helmet } from "react-helmet";
-import SectionOne from "../components/Contact/SectionOne";
-import SectionThree from "../components/Contact/SectionThree";
-import SectionTwo from "../components/Contact/SectionTwo";
-import SectionFive from "../components/Services/SectionFive";
-import Footer from "../components/Footer/Footer";
-import Navbar from "../components/Navbar/Navbar";
-import DrawerAppBar from "../components/Navbar/Appbar";
-import ContactInput from "../components/Contact/ContactInput";
 import FooterNew from "../components/Footer/FooterNew";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
-import DrawerAppBarWhite from "../components/Navbar/Appbar";
 import MainPage from "../components/Contact/MainPage";
 import AppBarNav from "../components/Navbar/Appbar";
 
-const Contact = (props) => {
-  const { isLoggedIn } = useContext(AuthContext);
-
+const Contact = () => {
   return (
     <>
-    <Helmet>
+      <Helmet>
         <title>Let's Elevate Your Online Presence Together</title>
         <meta
           name="description"
           content="Get in Touch with Prevail for Bespoke Marketing Solutions"
         />
       </Helmet>
-    <div style={{ width: "100%", overflowX: "hidden" }}>
-      <AppBarNav color="#000" />
-      <MainPage />
-      {/* <ContactInput /> */}
-      {/* <SectionThree /> */}
-      {/* <SectionFive /> */}
-      <FooterNew />
-    </div>
+      <div style={{ width: "100%", overflowX: "hidden" }}>
+        <AppBarNav color="#000" />
+        <MainPage />
+        <FooterNew />
+      </div>
     </>
   );
 };
