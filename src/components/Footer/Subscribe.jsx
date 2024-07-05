e842aa2216f4a21135a4fa4306b1647692b72a1c
sirlahm <riliwanademola@yahoo.com>
2024-07-05 12:07:34 +0100

    update

--- src/components/Footer/Subscribe.jsx
+++ src/components/Footer/Subscribe.jsx
@@ -9,7 +9,7 @@ import {
   OutlinedInput,
   Typography,
 } from "@mui/material";
-import React, { useState }from "react";
+import React, { useState } from "react";
 import axiosInstance from "../utils/axios";
 import makeToast from "../../makeToast";
 import * as yup from "yup";
@@ -23,7 +23,6 @@ const validationSchema = yup.object().shape({
 });
 
 const Subscribe = () => {
-
   const [isLoading, setIsLoading] = useState(false);
   const formik = useFormik({
     initialValues: {
@@ -31,14 +30,22 @@ const Subscribe = () => {
     },
     validationSchema: validationSchema,
     onSubmit: async (values, { resetForm }) => {
+      setIsLoading(true);
       try {
         await axiosInstance.post(`/api/auth/addSubscriber`, {
           email: values.email,
         });
         makeToast("success", "Successfully subscribed to our newsletters");
+        setIsLoading(false);
         resetForm();
       } catch (error) {
-        makeToast("error", error?.message?.title || "Something went wrong, Please try again later");
+        makeToast(
+          "error",
+          error?.message?.title ||
+            "Something went wrong, Please try again later"
+        );
+        setIsLoading(false);
+
         console.log(error);
       }
     },
@@ -140,7 +147,7 @@ const Subscribe = () => {
                           background: "#6E3EF4",
                         },
                       }}
-                      disabled={!(formik.isValid && formik.dirty)}
+                      disabled={!formik.isValid || formik.dirty || isLoading}
                     >
                       Subscribe
                     </Button>
@@ -200,9 +207,9 @@ const Subscribe = () => {
                   background: "#6E3EF4",
                 },
               }}
-              disabled={!(formik.isValid && formik.dirty)}
+              disabled={!formik.isValid || formik.dirty || isLoading}
             >
-              Subscribe!
+              Subscribe
             </Button>
           </Grid>
         </form>
