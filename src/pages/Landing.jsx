7d367b1a5187dcbf33b0d21dfee0c64a94c5caac
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-05 16:13:01 +0100

    cookies integration

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -42,6 +42,8 @@ const Landing = (props) => {
         <br />
         <JoinCustomers />
         <CookieConsent
+          // disableStyles
+          enableDeclineButton
           style={{
             background: "rgba(136, 78, 217,0.9)",
             height: "35vh",
@@ -50,7 +52,7 @@ const Landing = (props) => {
           }}
           location="bottom"
           buttonText="Accept"
-          cookieName="myAwesomeCookieName2"
+          cookieName="prevailagency"
           buttonStyle={{
             color: "#884ED9",
             fontSize: "18px",
@@ -59,6 +61,16 @@ const Landing = (props) => {
             borderRadius: "25px",
             background: "white",
             border: "1px solid rgba(136, 78, 217)",
+          }}
+          declineButtonText="Decline"
+          declineButtonStyle={{
+            color: "white",
+            fontSize: "18px",
+            width: "144px",
+            padding: "10px 0",
+            borderRadius: "25px",
+            background: "rgba(136, 78, 217, 0.9)",
+            border: "1px solid white",
           }}
           expires={150}
         >
