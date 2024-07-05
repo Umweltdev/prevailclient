f4210aa910ac047957aa9901acd3a1866ac41e99
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-05 15:55:18 +0100

    changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -15,8 +15,10 @@ import CookieConsent, {
   getCookieConsentValue,
 } from "react-cookie-consent";
 
+
 const Landing = (props) => {
   const { isLoggedIn } = useContext(AuthContext);
+  console.log(getCookieConsentValue("your_custom_cookie_name"));
 
   return (
     <div style={{ overflowX: "hidden" }}>
@@ -41,19 +43,15 @@ const Landing = (props) => {
         <br />
         <JoinCustomers />
         <CookieConsent
-          enableDeclineButton
           style={{
-            background: "rgba(136, 78, 217, 0.9)",
+            background: "rgba(136, 78, 217,0.9)",
             height: "35vh",
-            width: "100vw",
+            width: "70vw",
             display: "flex",
-
-            padding: "20px",
-            boxSizing: "border-box",
           }}
           location="bottom"
           buttonText="Accept"
-          cookieName="prevailagency"
+          cookieName="myAwesomeCookieName2"
           buttonStyle={{
             color: "#884ED9",
             fontSize: "18px",
@@ -62,37 +60,34 @@ const Landing = (props) => {
             borderRadius: "25px",
             background: "white",
             border: "1px solid rgba(136, 78, 217)",
-            margin: "10px", // Margin between buttons
-          }}
-          declineButtonText="Decline"
-          declineButtonStyle={{
-            color: "white",
-            fontSize: "18px",
-            width: "144px",
-            padding: "10px 0",
-            borderRadius: "25px",
-            background: "rgba(136, 78, 217, 0.9)",
-            border: "1px solid white",
-            margin: "10px", // Margin between buttons
           }}
           expires={150}
         >
           <p style={{ fontSize: "18px" }}>
-            We collect cookies to analyze our website traffic and performance;
-            we never collect any personal data.
+            use cookies to personalise content and ads, to provide social media
+            features and to analyse our traffic. We also share information about
+            your use of our site with our social media, advertising and
+            analytics partners who may combine it with other information that
+            you’ve provided to them or that they’ve collected from your use of
+            their services.{" "}
           </p>
+          {/* <span style={{ fontSize: "10px" }}>
+            This bit of text is smaller :O
+          </span> */}
+          {/* onAccept= */}
           {(acceptedByScrolling) => {
             if (acceptedByScrolling) {
+              // triggered if user scrolls past threshold
               alert("Accept was triggered by user scrolling");
             } else {
               alert("Accept was triggered by clicking the Accept button");
             }
           }}
+          {/* enableDeclineButton onDecline= */}
           {() => {
-            alert("Decline was triggered");
+            alert("nay!");
           }}
         </CookieConsent>
-
         <FooterNew />
       </div>
     </div>
