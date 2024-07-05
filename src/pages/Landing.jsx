5ef7f83a55f5f6d846040f9962c8fe314dbc41a4
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-05 15:55:18 +0100

    ridwan codemerge changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,6 +10,7 @@ import CoreValues from "../components/Landing/CoreValues";
 import { AuthContext } from "../context/AuthContext";
 import AppBarNav from "../components/Navbar/Appbar";
 import JoinCustomers from "../components/About/JoinOurCustomer";
+
 import CookieConsent, {
   Cookies,
   getCookieConsentValue,
@@ -18,6 +19,7 @@ import CookieConsent, {
 
 const Landing = (props) => {
   const { isLoggedIn } = useContext(AuthContext);
+  console.log(getCookieConsentValue("your_custom_cookie_name"));
 
   return (
     <div style={{ overflowX: "hidden" }}>
@@ -41,9 +43,55 @@ const Landing = (props) => {
         <br />
         <br />
         <JoinCustomers />
+        <CookieConsent
+          style={{
+            background: "rgba(136, 78, 217,0.9)",
+            height: "35vh",
+            width: "70vw",
+            display: "flex",
+          }}
+          location="bottom"
+          buttonText="Accept"
+          cookieName="myAwesomeCookieName2"
+          buttonStyle={{
+            color: "#884ED9",
+            fontSize: "18px",
+            width: "144px",
+            padding: "10px 0",
+            borderRadius: "25px",
+            background: "white",
+            border: "1px solid rgba(136, 78, 217)",
+          }}
+          expires={150}
+        >
+          <p style={{ fontSize: "18px" }}>
+            use cookies to personalise content and ads, to provide social media
+            features and to analyse our traffic. We also share information about
+            your use of our site with our social media, advertising and
+            analytics partners who may combine it with other information that
+            you’ve provided to them or that they’ve collected from your use of
+            their services.{" "}
+          </p>
+          {/* <span style={{ fontSize: "10px" }}>
+            This bit of text is smaller :O
+          </span> */}
+          {/* onAccept= */}
+          {(acceptedByScrolling) => {
+            if (acceptedByScrolling) {
+              // triggered if user scrolls past threshold
+              alert("Accept was triggered by user scrolling");
+            } else {
+              alert("Accept was triggered by clicking the Accept button");
+            }
+          }}
+          {/* enableDeclineButton onDecline= */}
+          {() => {
+            alert("nay!");
+          }}
+        </CookieConsent>
         <FooterNew />
       </div>
-    </div>
+    </div> 
   );
 };
 
