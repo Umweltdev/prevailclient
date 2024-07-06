9b2caa78529c02f80307f60c4f028333cf2b4b82
Suagrcothe <ifeanyivalentine82@gmail.com>
2024-07-06 13:52:47 +0100

    cookie coscent

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -10,27 +10,24 @@ import CoreValues from "../components/Landing/CoreValues";
 import { AuthContext } from "../context/AuthContext";
 import AppBarNav from "../components/Navbar/Appbar";
 import JoinCustomers from "../components/About/JoinOurCustomer";
-
-import CookieConsent, {
-  Cookies,
-  getCookieConsentValue,
-} from "react-cookie-consent";
+import CookieConsent from "react-cookie-consent";
+import { cookieConsentStyles, buttonStyle, declineButtonStyle } from "./styles";
 
 
 const Landing = (props) => {
   const { isLoggedIn } = useContext(AuthContext);
-  console.log(getCookieConsentValue("your_custom_cookie_name"));
 
   return (
     <div style={{ overflowX: "hidden" }}>
-      <Helmet>
-        <title>Prevail: Your Partner in Success</title>
-        <meta
-          name="description"
-          content="Prevail is a Strategic Partner for Businesses in the Digital Age."
-        />
-      </Helmet>
-      <div style={{ width: "100%", overflow: "hidden" }}>
+      <div style={{ overflowX: "hidden" }}>
+        <Helmet>
+          <title>Prevail: Your Partner in Success</title>
+          <meta
+            name="description"
+            content="Prevail is a Strategic Partner for Businesses in the Digital Age."
+          />
+        </Helmet>
+
         <AppBarNav color="#000" />
         <Hero />
         <SectionTwo />
@@ -44,54 +41,24 @@ const Landing = (props) => {
         <br />
         <JoinCustomers />
         <CookieConsent
-          style={{
-            background: "rgba(136, 78, 217,0.9)",
-            height: "35vh",
-            width: "70vw",
-            display: "flex",
-          }}
-          location="bottom"
+          enableDeclineButton
+          style={cookieConsentStyles}
           buttonText="Accept"
-          cookieName="myAwesomeCookieName2"
-          buttonStyle={{
-            color: "#884ED9",
-            fontSize: "18px",
-            width: "144px",
-            padding: "10px 0",
-            borderRadius: "25px",
-            background: "white",
-            border: "1px solid rgba(136, 78, 217)",
-          }}
+          cookieName="prevailagency"
+          buttonStyle={buttonStyle}
+          declineButtonText="Decline"
+          declineButtonStyle={declineButtonStyle}
           expires={150}
         >
-          <p style={{ fontSize: "18px" }}>
-            use cookies to personalise content and ads, to provide social media
-            features and to analyse our traffic. We also share information about
-            your use of our site with our social media, advertising and
-            analytics partners who may combine it with other information that
-            you’ve provided to them or that they’ve collected from your use of
-            their services.{" "}
+          <p style={{ fontSize: "18px", textAlign: "left" }}>
+            We use cookies to personalise content and ads. This website collects
+            cookies to deliver better user experience. we never collect any
+            personal data
           </p>
-          {/* <span style={{ fontSize: "10px" }}>
-            This bit of text is smaller :O
-          </span> */}
-          {/* onAccept= */}
-          {(acceptedByScrolling) => {
-            if (acceptedByScrolling) {
-              // triggered if user scrolls past threshold
-              alert("Accept was triggered by user scrolling");
-            } else {
-              alert("Accept was triggered by clicking the Accept button");
-            }
-          }}
-          {/* enableDeclineButton onDecline= */}
-          {() => {
-            alert("nay!");
-          }}
         </CookieConsent>
         <FooterNew />
       </div>
-    </div> 
+    </div>
   );
 };
 
