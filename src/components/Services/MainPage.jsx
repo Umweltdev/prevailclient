9947dfd21981690a7c419f9be71b42174b2709f2
mac <habeebmustapha96@gmail.com>
2024-07-10 11:04:16 +0100

    update

--- src/components/Services/MainPage.jsx
+++ src/components/Services/MainPage.jsx
@@ -2,13 +2,27 @@ import { Box, Grid, Typography } from "@mui/material";
 import React from "react";
 import ImageTextService from "./ImageTextService";
 import TextImageService from "./TextImageService";
+import { Transition } from 'react-transition-group';
+
+const duration = 300;
+
+const defaultStyle = {
+  transition: `opacity ${duration}ms ease-in-out`,
+  opacity: 0,
+};
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 1 },
+  exited: { opacity: 0 },
+};
 
 const MainPage = () => {
   return (
     <Grid
       sx={{
         pt: "176px",
-        // background: "linear-gradient(135deg, #DDD4EF, #F9FAFC, white)",
         background: "linear-gradient(135deg, #d1c4e9, #ffffff, #bbdefb)",
         "@media (max-width: 767px)": {
           pt: "168px",
@@ -66,49 +80,84 @@ const MainPage = () => {
           },
         }}
       >
-        <ImageTextService
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Branding_trrcgh.avif"
-          alt="Branding"
-          textHeader={"Brand Identity"}
-          text="Our “Brand Identity Package” service is the foundation for creating a lasting impression. We collaborate closely with you to define their unique brand identity, crafting eye-catching logos and compelling marketing collateral."
-          url="/brand-identity-package"
-        />
-        <TextImageService
-
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Search-Engine-Marketing_m1ebvn.avif"
-          alt="Search-Engine-Marketing"
-          textHeader={"Search Engine Marketing"}
-          text="Our “Search Engine Marketing” service offer tailored solutions to boost online visibility and engagement to support you throughout the dynamic digital realm."
-          url="/search-engine-marketing" //exe
-        />
-        <ImageTextService
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088569/Website-Development_gxpjci.avif"
-          alt="Website-Development"
-          textHeader={"Website Development"}
-          text="Discover the power of bespoke web solutions with Prevail and elevate your brand to new heights in the digital landscape. Transform, engage, and succeed effortlessly."
-          url="/website-development"
-        />
-        <TextImageService
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Custom-Website-Development_mvdf6w.avif"
-          alt="Custom-Website-Development"
-          textHeader={"Custom Website Development"}
-          text="Desire ownership over your digital identity and wish to convey your brand's story authentically and vividly, then our “custom website development” is perfect for you."
-          url="/custom-website-development"
-        />
-        <ImageTextService
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Marketing-Price-Displacement_omscxs.avif"
-          alt="Marketing-Price-Displacement"
-          textHeader={"Marketing Price Displacement"}
-          text="Unlock your business’s potential with our “Marketing Price Displacement” API. Harness advanced pricing strategies and yield management to optimise marketing spend, automate adjustments, and boost ROI.  "
-          url="/marketing-pricing-displacement"
-        />
-        <TextImageService
-          imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088562/Digital-Accelerator_p0j4ty.avif"
-          alt="Digital-Accelerator"
-          textHeader={"Digital Accelerator Bundle"}
-          text="Our “Digital Accelerator” bundle integrate cutting-edge website design, bespoke brand identity, and strategic SEO services to boost visibility engage customers, and enhance your digital footprint."
-          url="/digital-accelerator-bundle"
-        />
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <ImageTextService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Branding_trrcgh.avif"
+                alt="Branding"
+                textHeader={"Brand Identity"}
+                text="Our “Brand Identity Package” service is the foundation for creating a lasting impression. We collaborate closely with you to define their unique brand identity, crafting eye-catching logos and compelling marketing collateral."
+                url="/brand-identity-package"
+              />
+            </div>
+          )}
+        </Transition>
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <TextImageService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Search-Engine-Marketing_m1ebvn.avif"
+                alt="Search-Engine-Marketing"
+                textHeader={"Search Engine Marketing"}
+                text="Our “Search Engine Marketing” service offer tailored solutions to boost online visibility and engagement to support you throughout the dynamic digital realm."
+                url="/search-engine-marketing"
+              />
+            </div>
+          )}
+        </Transition>
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <ImageTextService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088569/Website-Development_gxpjci.avif"
+                alt="Website-Development"
+                textHeader={"Website Development"}
+                text="Discover the power of bespoke web solutions with Prevail and elevate your brand to new heights in the digital landscape. Transform, engage, and succeed effortlessly."
+                url="/website-development"
+              />
+            </div>
+          )}
+        </Transition>
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <TextImageService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Custom-Website-Development_mvdf6w.avif"
+                alt="Custom-Website-Development"
+                textHeader={"Custom Website Development"}
+                text="Desire ownership over your digital identity and wish to convey your brand's story authentically and vividly, then our “custom website development” is perfect for you."
+                url="/custom-website-development"
+              />
+            </div>
+          )}
+        </Transition>
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <ImageTextService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088563/Marketing-Price-Displacement_omscxs.avif"
+                alt="Marketing-Price-Displacement"
+                textHeader={"Marketing Price Displacement"}
+                text="Unlock your business’s potential with our “Marketing Price Displacement” API. Harness advanced pricing strategies and yield management to optimise marketing spend, automate adjustments, and boost ROI."
+                url="/marketing-pricing-displacement"
+              />
+            </div>
+          )}
+        </Transition>
+        <Transition in={true} timeout={duration} appear>
+          {(state) => (
+            <div style={{ ...defaultStyle, ...transitionStyles[state] }}>
+              <TextImageService
+                imageUrl="https://res.cloudinary.com/dtzuqacg3/image/upload/v1720088562/Digital-Accelerator_p0j4ty.avif"
+                alt="Digital-Accelerator"
+                textHeader={"Digital Accelerator Bundle"}
+                text="Our “Digital Accelerator” bundle integrate cutting-edge website design, bespoke brand identity, and strategic SEO services to boost visibility engage customers, and enhance your digital footprint."
+                url="/digital-accelerator-bundle"
+              />
+            </div>
+          )}
+        </Transition>
       </Box>
     </Grid>
   );
