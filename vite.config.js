e33d805526a3176099b4724d088d170d0f5c9194
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2025-08-28 15:50:38 +0100

    fix: fixing dependency issues

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), pluginRewriteAll()],
+    resolve: {
+    alias: {
+      '@babel/runtime/helpers/builtin/interopRequireDefault':
+        '@babel/runtime/helpers/interopRequireDefault.js',
+    },
+  },
   optimizeDeps: {
     include: ["@mui/material/Tooltip", "@emotion/styled"],
   },
