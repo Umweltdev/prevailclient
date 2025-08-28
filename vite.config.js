24faccf9f8288da148219ba820a69319356a9bc6
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2025-08-28 16:02:38 +0100

    adding code

--- vite.config.js
+++ vite.config.js
@@ -6,12 +6,12 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), pluginRewriteAll()],
-    resolve: {
-    alias: {
-      '@babel/runtime/helpers/builtin/interopRequireDefault':
-        '@babel/runtime/helpers/interopRequireDefault.js',
-    },
-  },
+  resolve: {
+  alias: {
+    '@babel/runtime/helpers/interopRequireDefault.js':
+      '@babel/runtime/helpers/interopRequireDefault'
+  }
+},
   optimizeDeps: {
     include: ["@mui/material/Tooltip", "@emotion/styled"],
   },
