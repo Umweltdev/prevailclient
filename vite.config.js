8f22a8cda8bfa1036c9b707b95367bb7d7c56d5a
Oluseyi Mayotomiwa Immanuel <simpayekini@simpa-yekini.local>
2025-08-30 14:17:35 +0100

    fix:fixinng runtime issues

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,12 @@ import pluginRewriteAll from "vite-plugin-rewrite-all";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr(), pluginRewriteAll()],
+  resolve: {
+  alias: {
+    '@babel/runtime/helpers/interopRequireDefault.js':
+      '@babel/runtime/helpers/interopRequireDefault'
+  }
+},
   optimizeDeps: {
     include: ["@mui/material/Tooltip", "@emotion/styled"],
   },
