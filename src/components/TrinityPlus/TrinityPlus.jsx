ba42a5aa301a302d3d08ed4c1187ac9d52bb34a9
Eugenex-umweltdev <eugene@umweltdev.com>
2025-08-31 13:27:16 +0100

    mcr -rcd

--- src/components/TrinityPlus/TrinityPlus.jsx
+++ src/components/TrinityPlus/TrinityPlus.jsx
@@ -1,13 +1,11 @@
-// import React from 'react'
-import { useEffect } from "react";
-import TrinityPlusPage from "./TrinityPlus/page.js"
+
 
 function TrinityPlus() {
-  useEffect(() => {
-        import("./TrinityPlus/styles/globals.css");
-    }, []);
+  // useEffect(() => {
+  //       import("./TrinityPlus/styles/globals.css");
+  //   }, []);
   return (
-    <TrinityPlusPage/>
+    <>TRinity plus </>
   )
 }
 
